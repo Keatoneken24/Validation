cf152fdff35aa00893da5334e4077da69e999127
Keatoneken24 <73285978+Keatoneken24@users.noreply.github.com>
2021-01-21 14:59:22 +0000

    Add files via upload

---
+++ Validation/Validation1/src/validation/Validation.java
@@ -0,0 +1,31 @@
+
+ 
+package validation;
+
+import java.util.*;
+ 
+ 
+public class Validation {
+
+   
+private static final Scanner input = new Scanner(System.in);
+
+public static void main(String[] args) {
+        
+double agetotal = 0;
+for(int i = 0 ; i<(10); i++){
+            
+            
+double age;
+System.out.println("Input age as int :");
+            
+agetotal = agetotal + input.nextDouble();
+            
+}
+        
+System.out.println((agetotal) / (10));
+
+}
+}
+    
+
